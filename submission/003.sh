f057708ffc11a4215c9bbbf58cb5748851ff6d52
Danilo Marcus <danilomarcus@users.noreply.github.com>
2024-11-23 16:58:08 -0300

    Update 003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,2 @@
 # How many new outputs were created by block 123,456?
+bitcoin-cli getblock $(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_064 -rpcpassword=UW1wGiCeFZRU getblockhash 123456) 2 | jq '[.tx[].vout[]] | length'
